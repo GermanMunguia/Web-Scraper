04582d3ec1051e0e9d4d11299472d03e59b8dc5c
GermanMunguia <munguiagerman48@gmail.com>
2021-01-10 23:15:29 -0500

    file

--- scraper.java
+++ scraper.java
@@ -4,9 +4,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.io.File; 
 
 public class scraper {
-	public static String[][] main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException {
 		
 		String[][] str = new String[50][4];  
 	     
@@ -21,24 +22,20 @@ public class scraper {
 	    int counter = 0; 
 	    //iterate through all 50 movies displayed on the site. 
 	    for (Element movie : movies) {
-	    	// Extract the title
-	        //String movieTitle = movie.getElementsByTag("a").text();
+	    	
+	    	//Extract the title, year, rating, and the description
 	        String movieDes = movie.getElementsByTag("p").text();
 	        String movieYear = movie.getElementsByClass("lister-item-year text-muted unbold").text();
 	        String movieRating = movie.getElementsByTag("strong").text();
 	        String movieTitle = movie.getElementsByAttributeValueStarting("href", "/title/").text();
-
-	     
-
-
-
-	        // Format and print the information to the console
-	        System.out.println(movieTitle + " ? " );
-	        System.out.println(movieYear );
-	        System.out.println(movieRating );
-	        System.out.println(movieDes + "\n\n" );
+	
+	        //Print
+	        System.out.println(movieTitle);
+	        System.out.println(movieYear);
+	        System.out.println(movieRating);
+	        System.out.println(movieDes + "\n" );
 	       
-	       
+	        //add it to the 2d array 
 	        str[counter][0] = movieTitle; 
 	        str[counter][1] = movieYear; 
 	        str[counter][2] = movieRating; 
@@ -47,7 +44,7 @@ public class scraper {
 	        
 	      }
 
-	    return str; 
+	    //return str; 
 	      
 	}
 }
