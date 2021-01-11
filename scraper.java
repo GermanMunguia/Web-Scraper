c6dc8344bb5331d98c3ac4635df560355362e7ef
GermanMunguia <munguiagerman48@gmail.com>
2021-01-10 22:45:12 -0500

    first commit

---
+++ scraper.java
@@ -0,0 +1,53 @@
+
+import java.io.IOException;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class scraper {
+	public static String[][] main(String[] args) throws IOException {
+		
+		String[][] str = new String[50][4];  
+	     
+		Document document = Jsoup.connect("https://www.imdb.com/search/title/?groups=top_250&sort=user_rating").get();
+
+	    //The website title
+	    System.out.println("Title: " +  document.title());
+
+	    //Get the element from the document 
+	    Elements movies = document.getElementsByClass("lister-item-content");
+	  
+	    int counter = 0; 
+	    //iterate through all 50 movies displayed on the site. 
+	    for (Element movie : movies) {
+	    	// Extract the title
+	        //String movieTitle = movie.getElementsByTag("a").text();
+	        String movieDes = movie.getElementsByTag("p").text();
+	        String movieYear = movie.getElementsByClass("lister-item-year text-muted unbold").text();
+	        String movieRating = movie.getElementsByTag("strong").text();
+	        String movieTitle = movie.getElementsByAttributeValueStarting("href", "/title/").text();
+
+	     
+
+
+
+	        // Format and print the information to the console
+	        System.out.println(movieTitle + " ? " );
+	        System.out.println(movieYear );
+	        System.out.println(movieRating );
+	        System.out.println(movieDes + "\n\n" );
+	       
+	       
+	        str[counter][0] = movieTitle; 
+	        str[counter][1] = movieYear; 
+	        str[counter][2] = movieRating; 
+	        str[counter][3] = movieDes; 
+	        counter++; 
+	        
+	      }
+
+	    return str; 
+	      
+	}
+}
